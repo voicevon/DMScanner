1a332104231654f1d20f1f3aa0b5700e9e0e9510
Xuming <voicevon@gmail.com>
2020-06-03 07:42:24 +0800

    got all camera list

--- slowdown.py
+++ slowdown.py
@@ -1,33 +1,60 @@
 import cv2
 from time import sleep
-cam = cv2.VideoCapture(0)
 
-cv2.namedWindow("test")
+class Camera_hub():
+    def __init__(self):
+        self.all_index = []
 
-img_counter = 0
+    def get_all_cameras(self):
+        # checks the first 10 indexes.
+        index = 0
+        i = 10
+        while i > 0:
+            cap = cv2.VideoCapture(index)
+            if cap.read()[0]:
+                print("get a new camera.  index = ", index)
+                self.all_index.append(index)
+                cap.release()
+            index += 1
+            i -= 1
 
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
-
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-        cv2.waitKey(1000)
+        print('=========================================')
+        print('get_all_cameras result: ', self.all_index)
         
-    sleep(0.5)
 
-cam.release()
+    def main(self):
+        cam = cv2.VideoCapture(0)
 
-cv2.destroyAllWindows()
+        cv2.namedWindow("test")
+
+        img_counter = 0
+
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("failed to grab frame")
+                break
+            cv2.imshow("test", frame)
+
+            k = cv2.waitKey(1)
+            if k%256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+            elif k%256 == 32:
+                # SPACE pressed
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                img_counter += 1
+                cv2.waitKey(1000)
+                
+            sleep(0.5)
+
+myhub = Camera_hub()
+myhub.get_all_cameras()
+
+
+# cam.release()
+
+# cv2.destroyAllWindows()
