99bb1fa2954c60d6be082b4bb392841779cc6c36
Xuming <voicevon@gmail.com>
2020-06-03 16:35:11 +0800

    rotated 180 degree of #4 and #6

---
+++ Hardware arrangement.md
@@ -0,0 +1,11 @@
+
+
+```
+Postion   Physical ID    OpenCV ID
+----------------------------------------------
+Top-Left        1           6
+Top-Right       2           4
+Bottom-Left     3           2
+Bottom-Right    4           0
+
+```
--- slowdown.py
+++ slowdown.py
@@ -23,19 +23,31 @@ class Camera_hub():
 
 
     def start_camera_and_grab_images(self,counter):
+        WIDTH = 2592
+        HEIGHT = 1944
+        center = (WIDTH / 2, HEIGHT / 2)
+        
+        scale = 1.0
+
         for index in self.all_index:
             this_cam = cv2.VideoCapture(index)
-            this_cam.set(cv2.CAP_PROP_FRAME_WIDTH, 2592)
-            this_cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1944)
+            this_cam.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+            this_cam.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
             cv2.namedWindow(str(index))
 
             ret, frame = this_cam.read()
             if ret:
+                if(index == 4 or index == 6):
+                    # rotate 180 degree
+                    M = cv2.getRotationMatrix2D(center, 180, scale)
+                    frame = cv2.warpAffine(frame, M, (WIDTH, HEIGHT)) 
+                
                 cv2.imshow(str(index),frame)
                 # write to file
                 img_name = './images/f_' + str(counter) + '_'+ str(index) + '.png'
                 cv2.imwrite(img_name, frame)
                 print("{} written!".format(img_name))
+
             else:
                 print("failed to grab frame")
             # prepare for next camera
