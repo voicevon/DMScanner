56c2a29122c618fed9497cc09b7c2039127fc4c4
Xuming <voicevon@gmail.com>
2020-06-03 08:07:21 +0800

    working version

--- slowdown.py
+++ slowdown.py
@@ -23,26 +23,29 @@ class Camera_hub():
 
 
     def start_camera_and_grab_images(self):
-        cam = cv2.VideoCapture(0)
-        index = 0
+        
+        for index in self.all_index:
+            this_cam = cv2.VideoCapture(index)
+            cv2.namedWindow(str(index))
 
-        cv2.namedWindow(str(index))
-
-        ret, frame = cam.read()
-        if ret:
-            cv2.imshow(str(index),frame)
-            img_name = "opencv_frame_{}.png".format(index)
-            cv2.imwrite(img_name, frame)
-            print("{} written!".format(img_name))
-        else:
-            print("failed to grab frame")
-
+            ret, frame = this_cam.read()
+            if ret:
+                cv2.imshow(str(index),frame)
+                img_name = "opencv_frame_{}.png".format(index)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+            else:
+                print("failed to grab frame")
+            # prepare for next camera
+            cv2.waitKey(1)
+            this_cam.release
 
 myhub = Camera_hub()
 myhub.get_all_cameras()
 
+cv2.namedWindow('splash')
+
 while True:
-    cv2.namedWindow('aa')
     k = cv2.waitKey(1)
     if k%256 == 27:
         # ESC pressed
@@ -50,6 +53,7 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
+        cv2.destroyWindow('splash')
         myhub.start_camera_and_grab_images()
 
 
